6292ad57ef4ec1ec8c7e8045a5c17aa253d1dabd
Noah Bolohan <noah.bolohan@gmail.com>
2024-10-08 10:08:57 -0400

    Multi-player spirit image

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ $(document).ready(
                         type : `player_${i}_name`,
                         placeholder : `Player ${i} name (optional)`
                     }
-                ).appendTo(`#div_player_${i}_name`);
+                ).appendTo(`#col_player_${i}_name`);
 
                 // Player spirit select
                 var spirit_select = $('<select>').attr(
@@ -24,7 +24,7 @@ $(document).ready(
                         name : `player_${i}_spirit`,
                         id : `select_player_${i}_spirit`
                     }
-                ).prop('required',true).appendTo(`#div_player_${i}_spirit`);
+                ).prop('required',true).appendTo(`#col_player_${i}_spirit`);
 
                 // Append the disabled default option
                 spirit_select.append(
@@ -51,7 +51,9 @@ $(document).ready(
                 // Hide columns for players 2+
                 if (i > 1) {
                     spirit_select.prop('required',false);
-                    $(`#col_player_${i}_info`).hide();
+                    $(`#col_player_${i}_name`).hide();
+                    $(`#col_player_${i}_spirit`).hide();
+                    $(`#col_player_${i}_spirit_image`).hide();
                 }
             }
         });
@@ -68,13 +70,16 @@ $(document).ready(
                     // Show columns for players <= this.value
                     if (i <= this.value) {
                         $(`#select_player_${i}_spirit`).prop("required",true);
-                        $(`#col_player_${i}_info`).show();
+                        $(`#col_player_${i}_name`).show();
+                        $(`#col_player_${i}_spirit`).show();
                     }
                     // Hide columns for players > this.value
                     else {
                         $(`#select_player_${i}_spirit`).prop("required",false);
-                        $(`#col_player_${i}_info`).hide();
                         $(`#select_player_${i}_spirit`).prop('selectedIndex',0);
+                        $(`#col_player_${i}_name`).hide();
+                        $(`#col_player_${i}_spirit`).hide();
+                        $(`#col_player_${i}_spirit_image`).hide();
                     }
                 }
             }
@@ -82,16 +87,62 @@ $(document).ready(
     }
 )
 
-// Set an event listener for showing spirit choice image by choosing a spirit
+// Set an event listener for showing player 1's spirit choice image by choosing a spirit for player 1
 $(document).ready(
     $(document).on(
         "change",
         "#select_player_1_spirit",
         function() {
-            $("#img_player_1_spirit_image").attr(
+            $("#col_player_1_spirit_image").attr(
                 "src",
                 `https://raw.githubusercontent.com/NoahBolohan/spirit-island-tracker/master/static/${$("#select_player_1_spirit").val().split(' ').join('_')}.png`
             );
+            $("#col_player_1_spirit_image").show();
+        }
+    )
+)
+
+// Set an event listener for showing player 2's spirit choice image by choosing a spirit for player 2
+$(document).ready(
+    $(document).on(
+        "change",
+        "#select_player_2_spirit",
+        function() {
+            $("#col_player_2_spirit_image").attr(
+                "src",
+                `https://raw.githubusercontent.com/NoahBolohan/spirit-island-tracker/master/static/${$("#select_player_2_spirit").val().split(' ').join('_')}.png`
+            );
+            $("#col_player_2_spirit_image").show();
+        }
+    )
+)
+
+// Set an event listener for showing player 3's spirit choice image by choosing a spirit for player 3
+$(document).ready(
+    $(document).on(
+        "change",
+        "#select_player_3_spirit",
+        function() {
+            $("#col_player_3_spirit_image").attr(
+                "src",
+                `https://raw.githubusercontent.com/NoahBolohan/spirit-island-tracker/master/static/${$("#select_player_3_spirit").val().split(' ').join('_')}.png`
+            );
+            $("#col_player_3_spirit_image").show();
+        }
+    )
+)
+
+// Set an event listener for showing player 4's spirit choice image by choosing a spirit for player 4
+$(document).ready(
+    $(document).on(
+        "change",
+        "#select_player_4_spirit",
+        function() {
+            $("#col_player_4_spirit_image").attr(
+                "src",
+                `https://raw.githubusercontent.com/NoahBolohan/spirit-island-tracker/master/static/${$("#select_player_4_spirit").val().split(' ').join('_')}.png`
+            );
+            $("#col_player_4_spirit_image").show();
         }
     )
 )
