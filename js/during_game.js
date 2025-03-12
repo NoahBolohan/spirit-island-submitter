95ffecce04ed7fa586fa5646d996aeb2d7c1ad13
Noah Bolohan <noah.bolohan@gmail.com>
2025-03-12 13:50:17 -0400

    CARDS_IN_PLAY align text-top

--- js/during_game.js
+++ js/during_game.js
@@ -491,7 +491,7 @@ function append_threshold_string(
                     $("<img>").attr(
                         {
                             src : "https://raw.githubusercontent.com/NoahBolohan/spirit-island-tracker/master/static/icons/power_card.svg",
-                            style: "height: 1em; display: inline-block; vertical-align: middle;"
+                            style: "height: 1em; display: inline-block; vertical-align: text-top;"
                         }
                     ).appendTo(
                         div
